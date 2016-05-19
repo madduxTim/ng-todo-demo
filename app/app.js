2db7648700126846145c9994550e3a45a6f14743
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-05-18 21:20:37 -0500

    Stopping place.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
 var app = angular.module("TodoApp", []);
+app.controller("NavCtrl", function($scope){
+    $scope.navItems = [{name: "All Items"}, {name:"New Item"}]
+})
 
 app.controller("TodoCtrl", function($scope){
     $scope.welcome = "hello";
