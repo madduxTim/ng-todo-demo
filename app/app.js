a7867a7461b1b828ffe5bb20ea6506b5d9e1d36f
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-05-21 10:11:52 -0500

    first function thrown in

--- app/app.js
+++ app/app.js
@@ -1,8 +1,18 @@
+"use strict";
 var app = angular.module("TodoApp", []);
 app.controller("NavCtrl", function($scope){
-    $scope.navItems = [{name: "All Items"}, {name:"New Item"}]
-})
+    $scope.navItems = [{name: "All Items"}, {name:"New Item"}];
+});
 
 app.controller("TodoCtrl", function($scope){
     $scope.welcome = "hello";
-})
+    $scope.showListView = true;
+    $scope.newItem = function(){
+        console.log("newItem");
+        $scope.showListView = false;
+    };
+    $scope.allItem = function(){
+        console.log("allItem");
+        $scope.showListView = true;
+    };
+});
