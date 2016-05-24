9e7e20796d14976057aba1436633c1f1d83eb300
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-05-23 20:57:02 -0500

    More stuff

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,4 +1,17 @@
 "use strict";
-app.controller("ItemViewCtrl", function($scope){
-    $scope.welcome = "hello";
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams){
+    $scope.items = [];
+    $scope.selectedItem = {};
+    console.log($routeParams.itemId);
+        $http.get("https://angular-tdm.firebaseio.com/items.json")  
+            .success(function(itemObject){
+                var itemCollection = itemObject;
+                Object.keys(itemCollection).forEach(function(key){
+                    itemCollection[key].id=key;
+                    $scope.items.push(itemCollection[key]);
+                    $scope.selectedItem = $scope.items.filter(function(item){
+                        return item.id === $routeParams.itemId;
+                    })[0];
+                })
+            });
 });
