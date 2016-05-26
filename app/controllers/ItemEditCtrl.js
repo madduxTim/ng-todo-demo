0ac4aab83805dfce830516a04911091a83a7f632
madduxTim <timmaddux@Tims-MacBook-Pro.local>
2016-05-25 21:02:45 -0500

    Editing added.

---
+++ app/controllers/ItemEditCtrl.js
@@ -0,0 +1,16 @@
+"use strict";
+app.controller("ItemEditCtrl", function($scope, $location, $routeParams, itemStorage){
+    $scope.newTask = {};
+      
+    itemStorage.getSingleItem($routeParams.itemId)
+        .then(function successCallback(response){
+            $scope.newTask=response;
+        })
+    $scope.addNewItem = function(){
+        itemStorage.updateItem($routeParams.itemId, $scope.newTask)
+            .then(function successCallback(response) {
+                console.log(response);
+                $location.url("/items/list");
+            });
+    };
+});
--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,17 +1,4 @@
 "use strict";
 app.controller("NavCtrl", function($scope){
-    $scope.navItems = [
-        {
-            name: "Logout",
-            url: '#/logout'
-        }, 
-        {
-            name: "All Items",
-            url: '#/items/list'
-        }, 
-        {
-            name:"New Item",
-            url:'#/items/new'
-        }
-    ];
+
 });
